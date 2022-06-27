5aaf03af673297925b690c60e29cc24acfdb21f3
puoya <puoya.tabaghi@gmail.com>
2022-06-27 15:12:24 -0500

    spca bug resolved

--- experiment 1/synthetic data.py
+++ experiment 1/synthetic data.py
@@ -6,15 +6,17 @@ import scipy.linalg
 ##############################################################
 class parameters:
     def __init__(self):
-        self.N = 10000 ## no. of points
-        self.D = 10 ## input dimension
-        self.d = 1 ## target dimension
-        self.sigma = 10 # noise std
+        self.N = 1000 ## no. of points
+        self.D = 100 ## input dimension
+        self.d = 9 ## target dimension
+        self.sigma = .1 # noise std
 param = parameters()
 ##############################################################
-X , S, noise_lvl = sfpca.random_spherical_data(param)
+X , S, noise_lvl_input = sfpca.random_spherical_data(param)
 X_, S_ = sfpca.estimate_spherical_subspace(X,param)
+noise_lvl_output = sfpca.compute_noise_lvl(X_,S)
 dist = sfpca.subspace_dist(S,S_)
+print(noise_lvl_input)
+print(noise_lvl_output)
 print(dist)
-print(noise_lvl)
 ##############################################################
